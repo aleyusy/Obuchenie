197418c234e75fc3395547e7c77c5b8df4fb8c10
Yury <aleyu@mail.ru>
2020-12-06 12:18:03 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// for (let i = 0; i < 2; i++) {
+//   console.log(i);
+// }
+// // console.log(i);
+// function ww(a, b) {
+//   let sum = a + b;
+//   console.log(sum);
+// }
+// ww(10, 20);
+
+// let ar = [1, 2, 3, 4];
+// let newArray = ar.map(function (number) {
+//   return number * number;
+// });
+
+// let newArray1 = ar.map((n) => n * n);
+
+// console.log(ar);
+// console.log(newArray);
+// console.log(newArray1);
+// if (10 > 1) {
+//   var a = 10;
+//   console.log(a);
+// }
+// console.log(a);
+// if (10 > 1) {
+//   var a = 10;
+//   a += 5;
+//   console.log(a);
+// }
+// console.log(a);
+
+//
+
+// randomWords[Math.floor(Math.random() * randomWords.length)];
+
+// var pickRandomWord = function (words) {
+//   return words[Math.floor(Math.random() * words.length)];
+// };
+var randomWords = [
+  'бесформенный',
+  'Пространство сознания',
+  'Истина',
+  'Нерождённый',
+  'Бессмертный',
+  'Сострадательный',
+  'Любящий',
+];
+
+let rDom = document.getElementById('rr');
+rDom.innerHTML =
+  'ТЫ ЕСТЬ - ' + randomWords[Math.floor(Math.random() * randomWords.length)];
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   personalMovieDB.movies[a] = b;
+// }
+// console.log(personalMovieDB);
+
+// let num = 50;
+// while (num <= 56) {
+//   console.log(num);
+//   num++;
+// }
+
+// let num = 50;
+// do {
+//   console.log(num);
+//   num++;
+// } while (num <= 55);
+
+// let num = 50;
+// for (let i = 1; i < 8; i++) {
+//   console.log(num);
+//   num++;
+// }
+// let num = 50;
+// for (let i = 1; i <= 7; i++) {
+//   if (i === 6) {
+//     // break;
+//     // continue;
+//   }
+//   console.log(i);
+// }
+// / const numberOfFilms = getElementById('col');
+// ("Сколько фильмов вы уже посмотрели?", "");
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt("На сколько оцените его?", "");
+//   personalMovieDB.movies[a] = b;
+// }
+// console.log(personalMovieDB);
